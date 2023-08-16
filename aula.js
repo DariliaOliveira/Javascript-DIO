f939f190b110d243e89c9771662043dd5e5f6fc3
Darília Oliveira <115103776+DariliaOliveira@users.noreply.github.com>
2023-08-16 11:17:03 -0300

    first commit

---
+++ aula.js
@@ -0,0 +1,11 @@
+// let variavel = 10;
+
+// variavel = 10/10
+
+// console.log(variavel);
+
+let precoCombustivel = 3.15;
+let gastoMedio = 0.75;
+let kmViagem = precoCombustivel / gastoMedio;
+
+console.log(kmViagem);
